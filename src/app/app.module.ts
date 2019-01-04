7ef04dafab361d62654107956cca1fa9832e000b
guozhenhua <1291091720@qq.com>
2019-01-04 17:16:20 +0800

    放弃covelent，采用material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,49 +1,24 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-
-import {AppComponent} from './app.component';
-// angular material
+// angular material lib
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {
-  MatButtonModule, MatListModule, MatIconModule, MatCardModule, MatMenuModule, MatInputModule, MatButtonToggleModule,
-  MatProgressSpinnerModule, MatSelectModule, MatSlideToggleModule, MatDialogModule, MatSnackBarModule, MatToolbarModule,
-  MatTabsModule, MatSidenavModule, MatTooltipModule, MatRippleModule, MatRadioModule, MatGridListModule,
-  MatDatepickerModule, MatNativeDateModule, MatSliderModule, MatAutocompleteModule
-} from '@angular/material';
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import {MatToolbarModule} from '@angular/material/toolbar';
+// local lib
+import {AppComponent} from './app.component';
+import {TopNavComponent} from './top-nav/top-nav.component';
 
-// covalent start
-import {CovalentLayoutModule} from '@covalent/core/layout';
-import {CovalentStepsModule} from '@covalent/core/steps';
-/* any other core modules */
-// (optional) Additional Covalent Modules imports
-import {CovalentHttpModule} from '@covalent/http';
-import {CovalentHighlightModule} from '@covalent/highlight';
-import {CovalentMarkdownModule} from '@covalent/markdown';
-import {CovalentDynamicFormsModule} from '@covalent/dynamic-forms';
-import {MenuComponent} from './menu/menu.component';
-
-// covalent end
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent,
+    TopNavComponent,
   ],
   imports: [
     BrowserModule,
-    CovalentLayoutModule,
-    CovalentStepsModule,
-    // angular material
     BrowserAnimationsModule,
-    MatButtonModule, MatListModule, MatIconModule, MatCardModule, MatMenuModule, MatInputModule, MatButtonToggleModule,
-    MatProgressSpinnerModule, MatSelectModule, MatSlideToggleModule, MatDialogModule, MatSnackBarModule, MatToolbarModule,
-    MatTabsModule, MatSidenavModule, MatTooltipModule, MatRippleModule, MatRadioModule, MatGridListModule,
-    MatDatepickerModule, MatNativeDateModule, MatSliderModule, MatAutocompleteModule,
-    // (optional) Additional Covalent Modules imports
-    CovalentHttpModule.forRoot(),
-    CovalentHighlightModule,
-    CovalentMarkdownModule,
-    CovalentDynamicFormsModule,
+    MatButtonModule, MatCheckboxModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/top-nav/top-nav.component.ts
@@ -0,0 +1,17 @@
+import {Component, OnInit} from '@angular/core';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+@Component({
+  selector: 'app-top-nav',
+  templateUrl: './top-nav.component.html',
+  styleUrls: ['./top-nav.component.css']
+})
+export class TopNavComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
